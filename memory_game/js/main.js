844e66421c9f811544c6adc7925b59448cd36e7b
gitwenlei <ngwenlei@gmail.com>
2019-06-23 20:57:30 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,29 @@
 console.log("Up and running!");
 
+/*
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "";
 var cardFour = "";
+*/
 
-console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardOne);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
